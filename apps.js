fc86be0d57700607ac67becdb52d633841b4cb2a
Suraj kumar <ksurajkumar336@gmail.com>
2026-07-02 15:55:52 +0530

    commit

---
+++ apps.js
@@ -0,0 +1,141 @@
+// let age = 23;
+// age = age + 3;
+// console.log(age);
+
+// const { useReducer } = require("react");
+
+// Average 
+
+// let hindi = 30;
+// let english = 70;
+// let math = 90;
+
+// let Average = (hindi + english + math) / 3;
+// console.log("Average of Marks : " + Average);
+
+
+// template literal 
+
+// let PencilePrice = 5;
+// let eresiorPrice = 10; 
+// // let output =" sum of total price is " + (PencilePrice + eresiorPrice) + " rupeese. ";
+
+// let output = `sum of total price is ${PencilePrice + eresiorPrice} rupess.`;
+// console.log(output);
+
+
+// If Statement 
+
+// let color = "red";
+
+// if(color === "red") {
+//   console.log("Stop");
+// }
+// if(color === "yellow"){
+//   console.log("Slow down");
+// }
+
+// if(color === "green") {
+//    console.log("go");
+// }
+
+// practice Question
+
+// let size = "M";
+
+// if(size === "XL") {
+//   console.log("Price is 250₹");
+
+// } else if (size === "L") {
+//   conaole.log("Price is 200₹")
+// } else if(size === "M") {
+//   console.log("Price is 100₹");
+// } else if(size === "S") {
+//   console.log("Price is 50₹");
+// } else {
+//   conaole.log("inavalid");
+// }
+
+
+
+
+// nested idf -else 
+
+// let marks = 99;
+
+// if(marks >= 33) {
+//   console.log("Pass");
+//   if(marks >=80 ) {
+//     console.log("Grade A");
+//   } else {
+//     console.log("Grade O");
+//   }
+// } else {
+//   console.log("Better Luck tyr next time !");
+// }
+
+// Question = 1
+
+// let str = "apple";
+// for(let i=0; i<=str.length; i++) {
+//   if(str[0] === "a" && str.length === 3) {
+//     console.log("Good String");
+//   } else {
+//     console.log("Not Good String");
+//   }
+// }
+
+
+
+// if(str[0] == "a" && str.length >= 3) {
+//   console.log("Good String");
+// } else {
+//   console.log("Not Good String");
+// }
+
+
+// switch statement 
+
+// let day = "10";
+
+// switch(day) {
+//   case "1" : 
+//   console.log("Monday");
+//   break;
+
+//   case "2" : 
+//   console.log("Tuesday");
+//   break;
+
+//   case "3" : 
+//   console.log("Wednesday");
+//   break;
+
+//   case "4" : 
+//   console.log("thirsday");
+//   break;
+
+//   case "5" : 
+//   console.log("friday");
+//   break;
+
+//   case "6" : 
+//   console.log("Saturday");
+//   break;
+
+//   case "7" : 
+//   console.log("Sunday");
+//   break;
+
+//   default :
+//   console.log("Wrong day");
+// }
+
+// Alert and prompt
+
+let firstName = prompt("Enter your first name :");
+let lastName = prompt("Enter your last name : ");
+
+let username = `My first name ${firstName} and last name ${lastName} : name is ${firstName}  ${lastName}`;
+
+console.log(alert(username));
